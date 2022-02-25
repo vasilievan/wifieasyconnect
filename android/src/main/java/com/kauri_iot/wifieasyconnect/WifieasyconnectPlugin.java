5aab515f4e88a79c44535a4c08c0791590db3495
aleksey <vasiliev.an@edu.spbstu.ru>
2022-02-25 15:24:35 +0300

    // some changes

--- android/src/main/java/com/kauri_iot/wifieasyconnect/WifieasyconnectPlugin.java
+++ android/src/main/java/com/kauri_iot/wifieasyconnect/WifieasyconnectPlugin.java
@@ -9,19 +9,21 @@ import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import android.os.Build;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.WifiManager;
 import androidx.annotation.RequiresApi;
 
 /** WifieasyconnectPlugin */
-public class WifieasyconnectPlugin implements FlutterPlugin, MethodCallHandler {
+public class WifieasyconnectPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private MethodChannel channel;
   private Context context;
+  private  Activity activity;
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
@@ -30,24 +32,38 @@ public class WifieasyconnectPlugin implements FlutterPlugin, MethodCallHandler {
     context = flutterPluginBinding.getApplicationContext();
   }
 
+  @Override
+  public void onDetachedFromActivity() {}
+
+  @Override
+  public void onAttachedToActivity(ActivityPluginBinding binding) {
+    activity = binding.getActivity();
+  }
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(ActivityPluginBinding binding) { }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() { }
+
   @RequiresApi(Build.VERSION_CODES.Q)
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
     if (call.method.equals("connect")) {
-      connect(context);
+      connect(context, activity);
     } else {
       result.notImplemented();
     }
   }
 
 
-  private void connect(Context context) {
+  private void connect(Context context, Activity activity) {
     final String INTENT_ACTION_WIFI_QR_SCANNER = "android.settings.WIFI_DPP_ENROLLEE_QR_CODE_SCANNER";
     WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
     if(wifiManager.isEasyConnectSupported())
     {
       final Intent intent = new Intent(INTENT_ACTION_WIFI_QR_SCANNER);
-      startActivityForResult(intent, 5000);
+      activity.startActivityForResult(intent, 5000);
     }
   }
 
